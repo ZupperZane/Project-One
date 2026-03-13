5bdb4b332e5c4d3146c0b8b86a8a23f5daab03b4
HAJ02 <Haj020206@gmail.com>
2026-03-13 12:19:31 -0400

    Routing Started, Navpage Tempalted

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,17 @@
+function Home(){
+    return (
+        <div className="NavPage">
+            <button>
+            Messages
+            </button>
+            <button>
+            Tasks
+            </button>
+            <button>
+            Settings
+            </button>
+        </div>
+    )
+}
+
+export default Home;
