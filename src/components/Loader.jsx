908f3cc17caedd429977ffe7b9bbeaf86fffe15c
Abachris <cmrburner@gmail.com>
2026-03-13 14:54:52 -0400

    rewrite

--- src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="min-h-[40vh] grid place-items-center">
-      <span className="loading loading-spinner loading-lg text-primary" />
-    </div>
-  );
-};
-
-export default Loader;
