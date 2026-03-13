db26fbf335cbb1fc0d7c325b5a3b4c064246324b
yasirdar19 <y.dar28@ncf.edu>
2026-03-13 14:34:10 -0400

    Add files via upload

---
+++ src/Components/Weather.tsx
@@ -0,0 +1,7 @@
+export function Weather(){
+    return (
+ <div>
+    Weather
+ </div>
+    )
+}
---
+++ src/Pages/Login.tsx
@@ -0,0 +1,14 @@
+export function Login(){
+    return (
+ <div>
+    <h1>
+        Please Login 
+    </h1>
+    <div>
+        <button>
+            Login Button Here
+        </button>
+    </div>
+ </div>
+    )
+}
